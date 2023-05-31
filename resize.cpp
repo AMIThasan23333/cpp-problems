ffbd2925a72d4e155ab2f4f8341289afa5423ce6
My Name <cpgsom819@gmail.com>
2023-05-31 21:03:11 +0800

    resize

---
+++ resize.cpp
@@ -0,0 +1,23 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+string s;
+
+cin >> s; 
+
+s.resize(5);
+
+s.resize(8, 'm');
+
+cout<<s.size() << endl;
+
+cout << s << endl;
+
+return 0;
+
+
+}
