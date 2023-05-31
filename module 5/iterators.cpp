2efc1dfb7b8c9b49a91e34f9cbd9997108a3feea
My Name <cpgsom819@gmail.com>
2023-05-31 22:11:17 +0800

    iterator

---
+++ module 5/iterators.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+    string s;
+
+    string :: iterator it;
+
+    cin >> s;
+
+
+for (it = s.begin(); it < s.end(); it++)
+{
+    cout << *it <<endl;
+    
+}
+
+
+
+
+return 0;
+}
