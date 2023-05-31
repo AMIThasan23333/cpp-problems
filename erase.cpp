979d1b1eb19a813e588fddc2b9a59a8336fcfc9a
My Name <cpgsom819@gmail.com>
2023-05-31 21:50:57 +0800

    modifiers

---
+++ erase.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+string a = "HelloWord";
+
+a.erase(4);
+
+a.erase(4, 1);
+
+cout<< a << endl;
+
+return 0;
+}
---
+++ replace.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+string a = "HelloWord";
+
+
+
+a.replace(4, 3, "soo");
+
+cout<< a << endl;
+
+return 0;
+}
