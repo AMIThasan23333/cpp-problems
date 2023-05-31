c35daf0ca2773f2e0f0aabaabb5c2dcdf624d715
My Name <cpgsom819@gmail.com>
2023-05-31 20:56:21 +0800

    clear()

---
+++ clear.cpp
@@ -0,0 +1,16 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+  string s = "hello";
+  cout<< s <<endl;
+  s.clear();
+  cout<< s << endl;
+  cout << s.size()<< endl;
+  
+
+return 0;
+}
---
+++ size.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+string s = "Hello World";
+
+cout<< s.size()<<endl;
+
+return 0;
+}
