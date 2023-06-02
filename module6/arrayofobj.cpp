5714233b34e09cdc37b01b77a4252b3434fd0bb0
My Name <cpgsom819@gmail.com>
2023-06-02 18:36:24 +0800

    sort function

--- module6/arrayofobj.cpp
+++ module6/arrayofobj.cpp
@@ -1,16 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
 class Student
 {
    public: 
    string name;
    int roll;
    int marks;
-
-
-
 };
+
+
+
 int main(){
+
   Student  a[2];
   for (int  i = 0; i < 2; i++)
   {
@@ -18,20 +21,20 @@ int main(){
     cin>>a[i].roll>> a[i].marks;
     cin.ignore();
   }
-
-
   for (int  i = 0; i < 2; i++)
   {
-
     cout<<a[i].name << " " << a[i].roll << " "<< a[i].marks<<endl;
-
   }
-  
-
 
 return 0;
+
 }
-/*  IF WE ARE USING ARRAY OF OBJECT WE CAN NOT GIVE COTSTRUCTOR  , BCZ ARRAY CAN NOT PASS ALL THE CONSTRUCTOR 
+
+
+
+/*  
+
+IF WE ARE USING ARRAY OF OBJECT WE CAN NOT GIVE COTSTRUCTOR  , BCZ ARRAY CAN NOT PASS ALL THE CONSTRUCTOR 
 
 //    Student(string name, int roll, int marks) {
 
